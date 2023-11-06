42cc8a443b9e49884df89216f946e69f50fdf24f
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-06 11:03:21 +0000

    Add files via upload

---
+++ for.1.py
@@ -0,0 +1,12 @@
+
+#this to calculate the sum and one of the way to add the number one after one
+count =0
+
+for i in [1,7,37,45,54,67,74]:
+     
+      count = count + i
+
+      print (,count)
+print ("the sum is = ",count)
+
+
